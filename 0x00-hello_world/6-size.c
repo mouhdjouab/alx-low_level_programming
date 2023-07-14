748af4d421249fb922e54e65db6c23e280d527c1
mouhdjouab <mouh.bw@gmail.com>
2023-07-14 04:28:53 +0200

    a

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints a line with puts function
+ * prints the size of various types on the computer with sizeof()
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+printf("Size of a char: %lu bytes(s)\n", sizeof(char));
+printf("Size of a int: %lu bytes(s)\n", sizeof(int));
+printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
+printf("Size of a float : %lu bytes(s)\n", sizeof(float));
+return (0);
+}
