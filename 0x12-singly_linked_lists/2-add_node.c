886534540fe26edf2002f5d62b33f17d5a0e55ad
mouhdjouab <mouh.bw@gmail.com>
2023-08-25 03:34:22 +0200

    end

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -24,7 +24,7 @@ list_t *add_node(list_t **head, const char *str)
 	while (str[len])
 		len++;
 
-	new = malloc(sizeof(list_t));
+	NodeNew = malloc(sizeof(list_t));
 
 	if (!NodeNew)
 		return (NULL);
