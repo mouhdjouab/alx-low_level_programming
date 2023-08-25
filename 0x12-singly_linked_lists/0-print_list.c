b658e7c7ce923b265ef97b7cc0d6a31b61bd4c6f
mouhdjouab <mouh.bw@gmail.com>
2023-08-25 05:45:05 +0200

    end

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,17 +8,28 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t x = 0;
+    size_t node_count = 0;
 
-	while (h)
-	{
-		if (!h->str)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", h->len, h->str);
-		h = h->next;
-		x++;
-	}
+    while (h)
+    {
+        if (!h->str)
+        {
+            printf("[0] (nil)\n");
+        }
+        else
+        {
+            size_t len = 0;
+            const char *str = h->str;
 
-	return (x);
+            while (str[len])
+                len++;
+
+            printf("[%zu] %s\n", len, h->str);
+        }
+
+        h = h->next;
+        node_count++;
+    }
+
+    return node_count;
 }
